34ce40d82f4ed9496455d642d6b75ce522e112f9
Swaraj9 <swaraj.dusane90@gmail.com>
2023-09-21 12:47:08 +0530

    Backend Changes 6

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/runmodel', (req, res) => {
   })
 });
 
-const port = 4000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
